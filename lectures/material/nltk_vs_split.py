e1b45a6cb2adda31164c5ccd4b7873aa96da55fa
Re'em Mor <reem.mor3@gmail.com>
2026-05-13 20:13:22 +0300

    Remove deprecated lecture materials and examples for NLP, including Titanic data analysis, NLTK usage, and RAG implementation.

--- lectures/material/nltk_vs_split.py
+++
@@ -1,8 +0,0 @@
-from nltk.tokenize import word_tokenize
-import nltk
-nltk.download('punkt', quiet=True)
-
-text = "Don't stop believing, hold on to that feeling"
-
-print("split():", text.split())
-print("word_tokenize():", word_tokenize(text))
